78716ae8d8a1604942127569d7c6492470750972
aosiname <osinamea@gmail.com>
2015-03-26 21:31:59 +0000

    add tests file

---
+++ TeaPartyKata/TeaPartyKata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace TeaPartyKata
+{
+    [TestClass]
+    public class TeaPartyKata
+    {
+        private SocialEvent se;
+
+        public TeaPartyKata()
+        {
+            se = new SocialEvent();
+        }
+
+        [TestMethod]
+        public void SocialEventHasGuestList()
+        {
+            var guestList = se.getGuestList();
+            Assert.IsInstanceOfType(guestList, typeof(List<string>));
+        }
+
+        [TestMethod]
+        public void addGuest()
+        {
+            se.addGuest("Andrew");
+            Assert.AreEqual(1, se.getGuestList().Count);
+        }
+
+        [TestMethod]
+        public void GuestListIsNotEmpty()
+        {
+            int countGuestList = se.getGuestList().Count;
+            Assert.IsTrue(countGuestList >= 0);
+        }
+    }
+}
